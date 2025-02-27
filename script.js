94d5917ca945f41427d1099268bc3367fb43bf70
Andre <andrevassoler66@gmail.com>
2025-02-27 00:29:04 -0300

    Descrição da mudança

--- script.js
+++ script.js
@@ -6,40 +6,59 @@ let vyBolinha = 10;
 let raio = diametro / 2;
 let xRaquete = 10;
 let yRaquete = 150;
-let comprimentoRaquete = 10
+let comprimentoRaquete = 10;
 let larguraRaquete = 100;
 
 function setup() {
-    createCanvas(600, 400);
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  desenhaBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  desenhaRaquete();
+  movimentaRaquete();
+  verificaColisaoRaquete();
+}
+
+function desenhaBolinha() {
+  circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha() {
+  xBolinha += vxBolinha;
+  yBolinha += vyBolinha;
+}
+
+function verificaColisaoBorda() {
+  if (xBolinha + raio > 600 || xBolinha - raio < 0) {
+    vxBolinha *= -1;
   }
-  
-  function draw() {
-    background(0);
-    circle(xBolinha, yBolinha, diametro);
-
-    xBolinha += vxBolinha;
-    yBolinha += vyBolinha;
-
-    if(xBolinha + raio > 600 || xBolinha - raio < 0) {
-        vxBolinha *= -1;
-    }
-    
-
-    if(yBolinha + raio > 400 || yBolinha - raio < 0) {
-      vyBolinha *= -1;
-    }
-
-    rect(xRaquete, yRaquete, comprimentoRaquete, larguraRaquete);
-    
-    if (keyIsDown(UP_ARROW) === true) {
-      yRaquete -= 10;
-    }
-
-    if (keyIsDown(DOWN_ARROW) === true) {
-      yRaquete += 10;
-    }
-
-
-
+  if (yBolinha + raio > 400 || yBolinha - raio < 0) {
+    vyBolinha *= -1;
   }
+}
+
+function desenhaRaquete() {
+  rect(xRaquete, yRaquete, comprimentoRaquete, larguraRaquete);
+}
+
+function movimentaRaquete() {
+  if (keyIsDown(UP_ARROW)) {
+    yRaquete -= 10;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    yRaquete += 10;
+  }
+}
+
+function verificaColisaoRaquete() {
+  if (xBolinha - raio < xRaquete + comprimentoRaquete && yBolinha - raio < yRaquete + larguraRaquete && yBolinha + raio > yRaquete) {
+    vxBolinha *= -1;
+  }
+}
+
+ 
 
